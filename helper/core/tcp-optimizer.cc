0c8c3835de7be5a8dc43ae47fe2e50deb5bf021b
Liao Wei-Ting <ws900908@gmail.com>
2025-10-08 15:17:25 +0800

    Change logic of disabling delayed ack.

--- helper/core/tcp-optimizer.cc
+++ helper/core/tcp-optimizer.cc
@@ -66,9 +66,12 @@ void TcpOptimizer::Generic() {
     Config::SetDefault("ns3::TcpSocketState::EnablePacing", BooleanValue(pacing_enabled));
 
     // Disable Delayed ACK by setting DelAckTimeout to 0
-    int64_t del_ack_timeout_ns = 0.2; // Default is typically 0.2s, set to 0 to disable delayed ACK
-    printf("  > Delayed ACK Timeout........ %.3f ms (0 disables delayed ACK)\n", del_ack_timeout_ns / 1e6);
-    Config::SetDefault("ns3::TcpSocket::DelAckTimeout", TimeValue(NanoSeconds(del_ack_timeout_ns)));
+    bool disable_delAck = false; // Default: false. Make it true to disable delayed ACK
+    if(disable_delAck){
+        int64_t del_ack_timeout_ns = 0; // Default is typically 0.2s, set to 0 to disable delayed ACK
+        printf("  > Delayed ACK Timeout........ %.3f ms (0 disables delayed ACK)\n", del_ack_timeout_ns / 1e6);
+        Config::SetDefault("ns3::TcpSocket::DelAckTimeout", TimeValue(NanoSeconds(del_ack_timeout_ns)));
+    }
 }
 
 void TcpOptimizer::OptimizeBasic(Ptr<BasicSimulation> basicSimulation) {
